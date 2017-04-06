2b557f783437d5171f7ef24361d7e53d29e36a06
Shuhei Kitagawa <shuhei.kitagawa@c-fo.com>
2017-04-07 08:57:22 +0900

    implemented parse and add translator class

--- parser.rb
+++ parser.rb
@@ -1,39 +1,42 @@
+require_relative './translator'
+
 class Parser
 
   COMMAND_TYPE = {a_command: :A_COMMAND, c_command: :C_COMMAND, l_command: :L_COMMAND}
 
-  def initialize(file_path)
+  attr_accessor :current_command, :command_type, :symbol
+  attr_reader :file, :code_class
+
+  def initialize(file_path, translator = Translator)
     @file = File.open(file_path)
+    @translator = Translator
     advance
   end
 
   def advance
     begin
-      @current_command = format_line(@file.readline)
+      self.current_command = format_line(file.readline)
+      parse
     rescue EOFError
-      @current_command = nil
-      @file.close
+      self.current_command = nil
+      file.close
       false
     end
   end
 
-  def current_command
-    @current_command
-  end
-
-  def command_type
-    @command_type
-  end
-
   def parse
-    if current_command.nil?
-      nil
-    elsif current_command.start_with?('@')
-      parse_a
-    elsif current_command.start_with?('(')
-      parse_l
+    if (m = current_command.match(/^@(\w+)/))
+      parse_a(m[1])
+    elsif (m = current_command.match(/^\((\w+)\)$/))
+      parse_l(m[1])
+    elsif (m = current_command.match(/^(\w+)=(\w+);(\w+)$/))
+      parse_c_full(m[1], m[2], m[3])
+    elsif (m = current_command.match(/^(\w+)=(\w+)$/))
+      parse_c_no_jmp(m[1], m[2])
+    elsif (m = current_command.match(/^(\w+);(\w+)$/))
+      parse_c_only_jmp(m[1], m[2])
     else
-      parse_c
+      raise InvalidCommandError "The line does not match any types of command: #{current_command}"
     end
   end
 
@@ -44,18 +47,31 @@ class Parser
     blank_removed.gsub(%r(//.*), '')
   end
 
-  def parse_a
-    @command_type = COMMAND_TYPE[:a_command]
+  def parse_a(symbol)
+    self.current_command = COMMAND_TYPE[:a_command]
+    self.symbol = symbol
+  end
+
+  def parse_l(symbol)
+    self.command_type = COMMAND_TYPE[:l_command]
+    self.symbol = symbol
+  end
+
+  def parse_c_full(dest, comp, jmp)
+    self.command_type = COMMAND_TYPE[:c_command]
 
   end
 
-  def parse_c
-    @command_type = COMMAND_TYPE[:c_command]
+  def parse_c_no_jmp(dest, comp)
+    self.command_type = COMMAND_TYPE[:c_command]
 
   end
 
-  def parse_l
-    @command_type = COMMAND_TYPE[:l_command]
+  def parse_c_only_jmp(comp, jmp)
+    self.command_type = COMMAND_TYPE[:c_command]
 
   end
+
 end
+
+class InvalidCommandError < StandardError; end
---
+++ translator.rb
@@ -0,0 +1,27 @@
+require 'singleton'
+
+# Translator is the same with the class which is called Code in the lecture
+# Translate C command into binary code
+class Translator
+  include Singleton
+
+  class << self
+
+    def translate_dest(dest)
+
+    end
+
+    def translate_comp(comp)
+
+    end
+
+    def translate_jmp(jmp)
+
+    end
+
+    private
+
+    
+
+  end
+end
