68fbf9cae64775341136009fb35a15c63e5dceb3
Shuhei Kitagawa <shuhei.kitagawa@c-fo.com>
2017-04-08 17:39:36 +0900

    inplemetend translate_jump and its test

--- spec/translator_spec.rb
+++ spec/translator_spec.rb
@@ -50,5 +50,83 @@ describe 'Translator' do
         expect(Translator.translate_dest('AMD')).to eq('111')
       end
     end
+
+    context 'if invalid dest is given' do
+      context 'if invalid character is given' do
+        it 'should raise InvalidDestinationError' do
+          expect{Translator.translate_dest('AMDW')}.to raise_error InvalidDestinationError
+        end
+      end
+
+      context 'if duplicate character is given' do
+        it 'should raise InvalidDestinationError' do
+          expect{Translator.translate_dest('AMDD')}.to raise_error InvalidDestinationError
+        end
+      end
+    end
+  end
+
+  describe '#translate_jump' do
+    context 'if jump == nil' do
+      it 'should return 000' do
+        expect(Translator.translate_jump(nil)).to eq('000')
+      end
+    end
+
+    context 'if jump == JGT' do
+      it 'should return 001' do
+        expect(Translator.translate_jump('JGT')).to eq('001')
+      end
+    end
+
+    context 'if jump == JEQ' do
+      it 'should return 010' do
+        expect(Translator.translate_jump('JEQ')).to eq('010')
+      end
+    end
+
+    context 'if jump == JGE' do
+      it 'should return 011' do
+        expect(Translator.translate_jump('JGE')).to eq('011')
+      end
+    end
+
+    context 'if jump == JLT' do
+      it 'should return 100' do
+        expect(Translator.translate_jump('JLT')).to eq('100')
+      end
+    end
+
+    context 'if jump == JME' do
+      it 'should return 101' do
+        expect(Translator.translate_jump('JME')).to eq('101')
+      end
+    end
+
+    context 'if jump == JLE' do
+      it 'should return 110' do
+        expect(Translator.translate_jump('JLE')).to eq('110')
+      end
+    end
+
+    context 'if jump == JMP' do
+      it 'should return 111' do
+        expect(Translator.translate_jump('JMP')).to eq('111')
+      end
+    end
+
+    context 'if invalid jump is given' do
+      context 'if invalid character is given' do
+        it 'should raise InvalidJumpError' do
+          expect{Translator.translate_jump('ABC')}.to raise_error InvalidJumpError
+        end
+      end
+
+      context 'if duplicate character is given' do
+        it 'should raise InvalidJumpError' do
+          expect{Translator.translate_jump('JMPP')}.to raise_error InvalidJumpError
+        end
+      end
+    end
   end
 end
--- src/translator.rb
+++ src/translator.rb
@@ -12,6 +12,10 @@ class Translator
         dest_hash = {d1: 0, d2: 0, d3: 0}
         next dest_hash if d.nil?
 
+        if invalid_dest?(dest)
+          raise InvalidDestinationError, "#{dest} is invalid form of destination"
+        end
+
         if d.include?(DESTINATION[:a_reg])
           dest_hash[:d1] = 1
         end
@@ -30,19 +34,70 @@ class Translator
 
     def translate_comp(comp)
 
+
     end
 
     def translate_jump(jump)
+      binarize(jump) do |j|
+        jump_hash = {j1: 0, j2: 0, j3: 0}
+        next jump_hash if j.nil?
 
+        case jump
+          when JUMP[:jgt]
+            jump_hash[:j1] = 0
+            jump_hash[:j2] = 0
+            jump_hash[:j3] = 1
+          when JUMP[:jeq]
+            jump_hash[:j1] = 0
+            jump_hash[:j2] = 1
+            jump_hash[:j3] = 0
+          when JUMP[:jge]
+            jump_hash[:j1] = 0
+            jump_hash[:j2] = 1
+            jump_hash[:j3] = 1
+          when JUMP[:jlt]
+            jump_hash[:j1] = 1
+            jump_hash[:j2] = 0
+            jump_hash[:j3] = 0
+          when JUMP[:jne]
+            jump_hash[:j1] = 1
+            jump_hash[:j2] = 0
+            jump_hash[:j3] = 1
+          when JUMP[:jle]
+            jump_hash[:j1] = 1
+            jump_hash[:j2] = 1
+            jump_hash[:j3] = 0
+          when JUMP[:jmp]
+            jump_hash[:j1] = 1
+            jump_hash[:j2] = 1
+            jump_hash[:j3] = 1
+          else
+            raise InvalidJumpError, "#{jump} is invalid form of jump"
+        end
+
+        jump_hash
+      end
     end
 
     private
 
     DESTINATION = {a_reg: 'A', d_reg: 'D', memory: 'M'}
+    JUMP = {jgt: 'JGT', jeq: 'JEQ', jge: 'JGE', jlt: 'JLT', jne: 'JME', jle: 'JLE', jmp: 'JMP'}
 
     def binarize(original)
       hash = yield(original)
       hash.reduce(''){|sum, pair| sum+pair[1].to_s}
     end
+
+    def invalid_dest?(dest)
+      chars = dest.split('')
+      # Each characters should be from one of the DESTINATION values
+      # Also they should not have duplicate characters
+      chars.any?{|c| !DESTINATION.values.include?(c)} ||
+        chars.length != chars.uniq.length
+    end
   end
 end
+
+class InvalidDestinationError < StandardError; end
+class InvalidJumpError < StandardError; end
