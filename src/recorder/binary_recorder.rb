aefd3c582a486e2e671cf80494b40c314415e886
Shuhei Kitagawa <shuhei.kitagawa@c-fo.com>
2017-04-12 09:59:06 +0900

    fix binary recorder to change file type to hack

--- src/recorder/binary_recorder.rb
+++ src/recorder/binary_recorder.rb
@@ -4,11 +4,23 @@ module Recorder
     attr_reader :file_path
 
     def initialize(file_path)
-      @file_path = file_path
+      @file_path = create_hack_path(file_path)
     end
 
     def register(binary)
       File.write(file_path, binary)
+    end
+    
+    private
+
+    def create_hack_path(file_path)
+      hack_file_path = file_path.gsub(/\.asm$/, '.hack')
+
+      if hack_file_path == file_path
+        raise "#{file_path} is invalid. You have to specify .asm file"
+      end
+
+      hack_file_path
     end
   end
 end
