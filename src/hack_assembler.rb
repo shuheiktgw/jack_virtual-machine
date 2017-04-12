76e5dd1c244d2eb4e45455325624b10adddc3ea6
Shuhei Kitagawa <shuhei.kitagawa@c-fo.com>
2017-04-12 10:29:28 +0900

    refactored hack assembler class

--- src/hack_assembler.rb
+++ src/hack_assembler.rb
@@ -5,22 +5,43 @@ require_relative './translator'
 
 class HackAssembler
 
-  class << self
+  attr_reader :file_path
 
-    def assemble(file_path)
-      binary_recorder = Recorder::BinaryRecorder.new(file_path)
-      translator = Translator.new(symbol_recorder(file_path))
-      binary_handler = Handler::BinaryHandler.new(translator, binary_recorder)
+  def initialize(file_path)
+    @file_path = file_path
+  end
 
-      binary_handler.execute
-    end
+  def assemble
+    binary_loader.execute
+  end
 
-    private
+  private
+  
+  def symbol_handler
+    Handler::SymbolHandler.new
+  end
 
-    def symbol_recorder(file_path)
-      symbol_handler = Handler::SymbolHandler.new
-      symbol_loader = Loader.new(file_path, symbol_handler)
-      symbol_loader.execute
-    end
+  def symbol_loader
+    Loader.new(file_path, symbol_handler)
+  end
+
+  def symbol_recorder
+    symbol_loader.execute
+  end
+
+  def binary_recorder
+    Recorder::BinaryRecorder.new(file_path)
+  end
+
+  def translator
+    Translator.new(symbol_recorder)
+  end
+
+  def binary_handler
+    Handler::BinaryHandler.new(translator, binary_recorder)
+  end
+
+  def binary_loader
+    Loader.new(file_path, binary_handler)
   end
 end
